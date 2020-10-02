cfb054e61326e654dcabc674ed565da776f43cc0
Arpit <reacharpit1801@gmail.com>
2020-10-02 17:04:19 +0530

    02-10-2020 changes

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Customer
+admin.site.register(Customer)
+
+from .models import Product
+admin.site.register(Product)
+
+from .models import Order
+admin.site.register(Order)
